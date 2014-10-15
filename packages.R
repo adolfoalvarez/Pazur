18ad12a0769826e019e5908b3e5dad10931db12f
adolfoalvarez <adalvarez+blog@gmail.com>
2014-10-15 11:51:57 +0200

    packages

---
+++ packages.R
@@ -0,0 +1,7 @@
+install.packages("devtools")
+devtools::install_github("hadley/lazyeval")
+devtools::install_github("hadley/dplyr")
+devtools::install_github("rstudio/ggvis", build_vignettes = FALSE)
+devtools::install_github("shiny", "rstudio")
+install.packages("ggplot2")
+install.packages("gcookbook")
